4c4e55b50671ad57629b576ee0e3a756a4d384f6
Kevin Ochieng <kevincliffo@gmail.com>
2020-04-01 22:33:17 +0300

    First Commit

---
+++ weather/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from . import views
+
+app_name = 'weather'
+
+urlpatterns = [
+    path('', views.index, name="home"),
+    path('delete/<city_name>/', views.delete_city, name="delete_city"),
+]
